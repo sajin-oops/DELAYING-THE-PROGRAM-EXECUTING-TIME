1c31b2a555b24d5ac4558350a5319cbd5f044235
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-04-17 19:42:01 +0530

    Add files via upload

---
+++ Deloying the program execution.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>Delaying the program execution for the given no of seconds</h2>";
+sleep(10);
+$name= "SAJIN";
+echo "<br>HELLO $name, welcome back after 5 seconds of time execution delay";
+
+?>
